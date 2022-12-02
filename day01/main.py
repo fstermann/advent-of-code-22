58d17e4013b137d6d27318d39132b458d65a51f5
Fabian Stermann <fabian.stermann@gmail.com>
2022-12-02 20:02:09 +0100

    add test for day01

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def read_input() -> str:
     with open("day01/input.txt", "r") as f:
         return f.read()
@@ -5,16 +8,47 @@ def read_input() -> str:
 
 def puzzle1(input_: str) -> int:
     elves_calories = input_.split(sep="\n\n")
-    return max(sum(int(cal) for cal in cals.split("\n")) for cals in elves_calories)
+    return max(sum(int(cal) for cal in cals.splitlines()) for cals in elves_calories)
 
 
 def puzzle2(input_: str) -> int:
     elves_calories = input_.split(sep="\n\n")
     return sum(
-        sorted(sum(int(cal) for cal in cals.split("\n")) for cals in elves_calories)[
+        sorted(sum(int(cal) for cal in cals.splitlines()) for cals in elves_calories)[
             -3:
         ]
     )
+
+
+INPUT = """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+EXPECTED_1 = 24000
+EXPECTED_2 = 45000
+
+
+@pytest.mark.parametrize(
+    ("input_", "expected", "puzzle"),
+    (
+        (INPUT, EXPECTED_1, puzzle1),
+        (INPUT, EXPECTED_2, puzzle2),
+    ),
+)
+def test_puzzle(input_, expected, puzzle):
+    assert puzzle(input_) == expected
 
 
 if __name__ == "__main__":
