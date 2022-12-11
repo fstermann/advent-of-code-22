f29456150c15e7ba64302156d752cb7a149d51ba
Fabian Stermann <fabian.stermann@gmail.com>
2022-12-11 10:16:10 +0100

    add day10

---
+++ day10/main.py
@@ -0,0 +1,229 @@
+from __future__ import annotations
+
+import os
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def read_input() -> str:
+    with open(INPUT_TXT) as f:
+        return f.read()
+
+
+def puzzle1(input_: str) -> int:
+    cycle = 0
+    x = 1
+    signal = 0
+
+    for line in input_.splitlines():
+        cycle += 1
+        if (cycle - 20) % 40 == 0:
+            signal += cycle * x
+
+        if line.startswith("addx"):
+            _, value = line.split()
+            cycle += 1
+            if (cycle - 20) % 40 == 0:
+                signal += cycle * x
+
+            x += int(value)
+
+    return signal
+
+
+def puzzle2(input_: str) -> str:
+    cycle = 0
+    x = 1
+    crt = [["."] * 40 for _ in range(6)]
+
+    for line in input_.splitlines():
+        cycle += 1
+
+        if (cycle - 1) % 40 in [x - 1, x, x + 1]:
+            crt[(cycle - 1) // 40][(cycle - 1) % 40] = "#"
+
+        if line.startswith("addx"):
+            _, value = line.split()
+            cycle += 1
+            if (cycle - 1) % 40 in [x - 1, x, x + 1]:
+                crt[(cycle - 1) // 40][(cycle - 1) % 40] = "#"
+            x += int(value)
+
+    return "\n".join("".join(row) for row in crt)
+
+
+INPUT = """\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+"""
+EXPECTED_1 = 13140
+EXPECTED_2 = """\
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."""
+
+
+@pytest.mark.parametrize(
+    ("input_", "expected", "puzzle"),
+    (
+        (INPUT, EXPECTED_1, puzzle1),
+        (INPUT, EXPECTED_2, puzzle2),
+    ),
+)
+def test_puzzle(input_, expected, puzzle):
+    assert puzzle(input_) == expected
+
+
+if __name__ == "__main__":
+    input_ = read_input()
+    print(puzzle1(input_))
+    print(puzzle2(input_))
